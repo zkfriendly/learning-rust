9748c076a2f524cc255b2220ef9da75835935996
zkFriendly <zkfriendly@skiff.com>
2023-12-25 12:20:06 +0100

    implement sum and prod traits for stack

--- stack/src/main.rs
+++ stack/src/main.rs
@@ -1,3 +1,18 @@
+use stack::Stack;
+
+use crate::stack::StackOps;
+
+mod stack;
+
 fn main() {
-    println!("Hello, world!");
+    let mut stack: Stack<u32> = Stack::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(4);
+
+    let sum = stack.sum();
+    let prod = stack.prod(1);
+
+    println!("sum: {:?}, prod: {:?}", sum, prod);
 }
---
+++ stack/src/stack/mod.rs
@@ -0,0 +1,61 @@
+use std::clone;
+use std::fmt::Display;
+use std::ops::{Add, Mul};
+
+#[derive(Debug)]
+pub struct Stack<T> {
+    data: Vec<T>,
+}
+
+impl<T> Stack<T> {
+    pub fn new() -> Stack<T> {
+        Stack { data: Vec::new() }
+    }
+
+    pub fn push(&mut self, item: T) {
+        self.data.push(item);
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        self.data.pop()
+    }
+
+    pub fn peek(&self) -> Option<&T> {
+        self.data.last()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.data.len() == 0
+    }
+}
+
+impl<T> StackOps<T> for Stack<T>
+where
+    T: Default + clone::Clone + Add<Output = T> + Mul<Output = T>,
+{
+    fn sum(&self) -> Option<T> {
+        if self.is_empty() {
+            None
+        } else {
+            Some(
+                self.data
+                    .iter()
+                    .cloned()
+                    .fold(Default::default(), |acc, x| acc + x),
+            )
+        }
+    }
+
+    fn prod(&self, init: T) -> Option<T> {
+        if self.is_empty() {
+            None
+        } else {
+            Some(self.data.iter().cloned().fold(init, |acc, x| acc * x))
+        }
+    }
+}
+
+pub trait StackOps<T> {
+    fn sum(&self) -> Option<T>;
+    fn prod(&self, init: T) -> Option<T>;
+}
